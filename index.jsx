8a851ca4f57da9945e79d8fe8b7458b981a0187b
Daniel Hyres <danhyres@gmail.com>
2023-03-07 14:09:58 -0500

    main push

--- index.jsx
+++ index.jsx
@@ -1,33 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+/**
+ * Challenge:
+ * Bootstrap the VanLife project by creating the first 2 routes:
+ * Home and About.
+ * 
+ * Also include the navbar that can link between the two routes.
+ * For now, you'll either need to copy/paste the navbar code
+ * to both Home and About pages, or you'll need to find a place
+ * to put it where it can be shared between the two pages.
+ * (Don't overthink this part - just do whatever is easiest for
+ * you because we'll learn a better approach very soon)
+ * 
+ * Review challenge: do all the CSS yourself based on the design
+ * linked in the slides.
+ */
 
 function App() {
-  return (    
-    <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
-function Home() {
   return (
-    <h1>Hello world!</h1>
+    <h1>Start here</h1>
   )
 }
 
-function About() {
-  return (
-    <h1>About page!</h1>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root'))
-  .render(<App />); 
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
