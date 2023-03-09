67e6b89b469a5fc282d21fe79689717d2d0347c5
Daniel Hyres <danhyres@gmail.com>
2023-03-09 14:21:29 -0500

    added fetch request with URL parameters and conditional rendering

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-
 function VanDetail(props) {
   
   const params = useParams()
-  console.log(params)
+  const [vanDetails, setVanDetails] = React.useState([])
+
+  React.useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then(res => res.json())
+      .then(data => setVanDetails(data.vans))
+
+  }, [params.id])
+  console.log(vanDetails)
+  
   return (
-    <div>VanDetail page goes here</div>
+    <div className='van-detail-container'>
+      {vanDetails ? (
+        <div className="van-detail">
+          <img src={vanDetails.imageUrl} />
+          <i className={`van-type ${vanDetails.type} selected`}>{vanDetails.type}</i>
+          <h2>{vanDetails.name}</h2>
+          <p className='van-price'>${vanDetails.price}<span>/day</span></p>
+          <p>{vanDetails.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
+        ) : <h2>Loading...</h2>
+      }
+
+    </div>
   )
 }
 
