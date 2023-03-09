9ca7c29f4e3472fa7cf2e41e9f37a278197b2397
Daniel Hyres <danhyres@gmail.com>
2023-03-09 13:41:21 -0500

    added useParams for retrieving params from URL

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 
 function VanDetail(props) {
+  
+  const params = useParams()
+  console.log(params)
   return (
     <div>VanDetail page goes here</div>
   )
 }
-
 
 export default VanDetail
