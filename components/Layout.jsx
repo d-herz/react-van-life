90e68941dc38aab49710384aad51b6bb06150c1c
Daniel Hyres <danhyres@gmail.com>
2023-03-11 14:18:46 -0500

    Added NavLink components for dynamic navigation styling

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import Header from './Header';
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 function Layout(props) {
   return (
-    <>
+    <div className='site-wrapper'>
       <Header />
       <Outlet />
+      <Footer />
     
-    </>
+    </div>
   )
 }
-
 
 export default Layout
