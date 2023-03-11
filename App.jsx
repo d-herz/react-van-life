dbbc164dbb275f1caaae38ebc54c12aab3590f0d
Daniel Hyres <danhyres@gmail.com>
2023-03-11 12:20:35 -0500

    Added Hostlayout component and routes

--- App.jsx
+++ App.jsx
@@ -8,6 +8,7 @@ import Vans from './pages/Vans/Vans';
 import VanDetail from './pages/Vans/VanDetail';
 import Layout from './components/Layout';
 import HostDashboard from './pages/Host/HostDashboard';
+import HostLayout from "./pages/Host/HostLayout";
 import Income from './pages/Host/Income';
 import Reviews from "./pages/Host/Reviews";
 
@@ -18,12 +19,15 @@ export default function App() {
       <Routes>
         <Route element={<Layout/>}>
           <Route path='/' element={<Home />} />
-          <Route path='/host' element={<HostDashboard />} />
-          <Route path='/host/income' element={<Income />} />
-          <Route path='/host/reviews' element={<Reviews />} />
           <Route path='/about' element={<About />} />
           <Route path='/vans' element={<Vans />}/>
           <Route path='/vans/:id' element={<VanDetail />}/>
+          <Route path='/host' element={<HostLayout />} >
+            <Route path='/host' element={<HostDashboard />} />
+            <Route path='/host/income' element={<Income />} />
+            <Route path='/host/reviews' element={<Reviews />} />
+          </Route>
+
         </Route>
       </Routes>
     </BrowserRouter>
