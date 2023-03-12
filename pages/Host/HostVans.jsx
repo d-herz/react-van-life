bf20ce10564a30278db2fac71f73de71d21c6a83
Daniel Hyres <danhyres@gmail.com>
2023-03-12 14:19:00 -0400

    Updated link paths from absolute to relative

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -15,27 +15,37 @@ function HostVans(props) {
 
   const hostVansCard = hostVans.map(van => {
     return (
-      <div key={van.id} className="van-tile">
-        <Link to={`/host/vans/${van.id}`}>
-          <img src={van.imageUrl} />
-          <div className="van-info">
+      <Link
+        to={`/host/vans/${van.id}`}
+        key={van.id}
+        className="host-van-link-wrapper"
+      >
+        <div className="host-van-single" key={van.id}>
+          <img src={van.imageUrl} alt={`Photo of ${van.name}`}/>
+          <div className="host-van-info">
             <h3>{van.name}</h3>
-            <p>${van.price}<span>/day</span></p>
-          </div>
-          <i className={`van-type ${van.type} selected`}>{van.type}</i>
-        </Link>
-
-      </div>
+            <p>${van.price}/day</p>
+          </div>          
+        </div>
+      </Link>
     )
   })
 
   return (
-    <div className="van-list-container">
-      <h1>Your listed vans</h1>
-      <div className="van-list">
-        {hostVansCard}
+    <section>
+      <h1 className='host-vans-title'>Your listed vans</h1>
+      <div className="host-van-list">
+        {
+          hostVans.length > 0 ? (
+            <section>
+              {hostVansCard}
+            </section>
+          ) : (
+              <h2>Loading...</h2>
+          )
+        }
       </div>
-    </div>
+    </section>
   )
 }
 
