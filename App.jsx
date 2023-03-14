e243061360d36578b5e73344c83459a82d36e013
Daniel Hyres <danhyres@gmail.com>
2023-03-14 17:05:23 -0400

    Updated browserRouter syntax to newer setup, created loader function, deleted uneccesary imports (BrowserRouter, Routes)

--- App.jsx
+++ App.jsx
@@ -1,10 +1,15 @@
 import "./server"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Vans from './pages/Vans/Vans';
+import Vans, { loader as vansLoader} from './pages/Vans/Vans';
 import VanDetail from './pages/Vans/VanDetail';
 import Layout from './components/Layout';
 import HostDashboard from './pages/Host/HostDashboard';
@@ -19,33 +24,33 @@ import Reviews from "./pages/Host/Reviews";
 import NotFound from "./pages/NotFound";
 
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Layout />}>
+    <Route index element={<Home />} />
+    <Route path='about' element={<About />} />
+    <Route path='vans' element={<Vans />} loader={vansLoader}/>
+    <Route path='vans/:id' element={<VanDetail />} />
+
+    <Route path='host' element={<HostLayout />} >
+      <Route index element={<HostDashboard />} />
+      <Route path='income' element={<Income />} />
+      <Route path='reviews' element={<Reviews />} />
+      <Route path='vans' element={<HostVans />} />
+
+      <Route path='vans/:id' element={<HostVanDetails />} >
+        <Route index element={<HostVanInfo />} />
+        <Route path="pricing" element={<HostVanPricing />} />
+        <Route path="photos" element={<HostVanPhotos />} />
+      </Route>
+    </Route>
+
+    <Route path="*" element={<NotFound />} />
+  </Route>
+))
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='vans' element={<Vans />}/>
-          <Route path='vans/:id' element={<VanDetail />} />
-          
-          <Route path='host' element={<HostLayout />} >
-            <Route index element={<HostDashboard />} />
-            <Route path='income' element={<Income />} />
-            <Route path='reviews' element={<Reviews />} />
-            <Route path='vans' element={<HostVans />} />
-
-            <Route path='vans/:id' element={<HostVanDetails />} >
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
-            </Route>
-          </Route>
-
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   )
 }
 
