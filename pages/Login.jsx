4bb93933783d42ab9c5a467121874b3b91c38665
Daniel Hyres <danhyres@gmail.com>
2023-03-17 17:53:16 -0400

    Updated action function to handle errors via try catch, removed previous error state, updated conditional error rendering to use new error

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,61 +1,66 @@
 import React from "react"
-import { useNavigate, useLocation, Form } from "react-router-dom";
+import { useNavigate, useLocation, Form, useActionData } from "react-router-dom";
 import { loginUser } from "../api";
 
 
-export async function action( { request }) {
-  const formData = await request.formData()
-  const email = formData.get("email")
-  const password = formData.get("password")
+export async function action({ request }) {
 
-  console.log(email, password)
-  // Process this info however
-}
+  try { 
+    const formData = await request.formData()
+    const email = formData.get("email")
+    const password = formData.get("password")
+    const data = await loginUser({ email, password })
+    console.log(data)
+    return data
+  } catch(err) {
+    return {
+      error: err.message
+    }
+
+  }
+    
+} 
 
 export default function Login() {
-  // const [loginFormData, setLoginFormData] = React.useState(
-  //   {
-  //     email: "",
-  //     password: ""
-  //   }
-  // )
+ 
 
   const [status, setStatus] = React.useState("idle")
-  const [error, setError] = React.useState(null)
+  // const [error, setError] = React.useState(null)
 
+  const data = useActionData()
   const location = useLocation()
   // console.log(location)
-
   const navigate = useNavigate();
+
 
   let from = location.state?.from?.pathname || "/host";
 
 
-  // function handleSubmit(e) {
-  //   e.preventDefault()
-  //   setStatus("submitting")
-  //   setError(null) 
+  function handleSubmit() {
+    // e.preventDefault()
+    // setStatus("submitting")
+    // setError(null)
 
-  //   loginUser(loginFormData)
-  //     .then(data => {
-  //       localStorage.setItem("loggedin", true)
-  //       navigate(from, { replace: true })
-  //     })
-  //     .catch(err => {
-  //       setError(err)
-  //     })
-  //     .finally(() => {
-  //       setStatus("idle")
-  //     })
-  // }
+    // navigate(from, { replace: true })
 
-  // function handleChange(e) {
-  //   const { name, value } = e.target
-  //   setLoginFormData(prev => ({
-  //     ...prev,
-  //     [name]: value
-  //   }))
-  // }
+
+    // loginUser(loginFormData)
+    //   .then(data => {
+    //     localStorage.setItem("loggedin", true)
+        // navigate(from, { replace: true })
+    //   })
+    //   .catch(err => {
+    //     setError(err)
+    //   })
+    //   .finally(() => {
+    //     setStatus("idle")
+    //   })
+  }
+
+  if (data?.token) {
+    navigate(from, { replace: true })
+  }
+
 
   return (
     <div className="login-container">
@@ -65,8 +70,8 @@ export default function Login() {
       }
       <h1>Sign in to your account</h1>
       {
-        error &&
-        <h3 className="login-error">{error.message}</h3>
+        data?.error &&
+        <h3 className="login-error">{data.error}</h3>
       }
       <Form className="login-form" action="/login" method="post">
         <input
