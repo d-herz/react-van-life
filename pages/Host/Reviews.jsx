1b4a157e636a108530772f1ef750ea44c9a5f610
Daniel Hyres <danhyres@gmail.com>
2023-03-21 12:18:14 -0400

    Fixed image import

--- pages/Host/Reviews.jsx
+++ pages/Host/Reviews.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { BsStarFill } from "react-icons/bs"
-import { ReviewGraph } from "../../assets/images/reviews-graph.png"
+import ReviewGraph  from "../../assets/images/reviews-graph.png"
 
 export default function Reviews() {
   const reviewsData = [
